9efbd836667a646cc2cf86ddf61399d6240d34a4
brederle <myclockpi@t-online.de>
2018-11-14 18:40:01 +0100

    Add brightness detection and change

--- src/simpledigiclock/simpledigiclock.py
+++ src/simpledigiclock/simpledigiclock.py
@@ -2,54 +2,77 @@ from datetime import datetime
 from kivy.lang import Builder
 from kivy.logger import Logger
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, ListProperty
 
 from myclockpi.iclockface import IClockFace
 
-
 class SimpleDigiClockLayout(BoxLayout):
-    clockValue   = ObjectProperty(None)
-    dateValue    = ObjectProperty(None)
+    clockValue  = ObjectProperty(None)
+    dateValue   = ObjectProperty(None)
+    clockColor  = ListProperty(None)
+    frontColor  = ListProperty(None)
+    # alarmState  = StringProperty(None) 
 
-    def __init__(self, filename, **kwargs):
-        Builder.load_file("./simpledigiclock/" + filename)
-        super(BoxLayout,self).__init__(**kwargs)
-        Logger.debug("Clock digit format: ", self.clockValue)
-        self.clockFormat  = self.clockValue.text 
-        self.dateFormat   = self.dateValue.text
+    def __init__(self):
+        super().__init__()
+        #Logger.debug("Clock digit format: ", self.clockValue.text)
+        self.clockFormat = self.clockValue.text 
+        self.dateFormat  = self.dateValue.text
+
+
+Builder.load_file("./simpledigiclock/simpledigiclock_l.kv")
+
+class SimpleDigiClockLandscape(SimpleDigiClockLayout):
+
+    def __init__(self):
+        super().__init__()
+
+
+
+Builder.load_file("./simpledigiclock/simpledigiclock_p.kv")
+
+class SimpleDigiClockPortrait(SimpleDigiClockLayout):
+
+    def __init__(self):
+        super().__init__()
+
 
 class SimpleDigiClock(IClockFace):
-
     def __init__(self):
         '''
         Init simpledigiclock with a portrait and a landscape layout
         '''
-        portraitLayout  = SimpleDigiClockLayout(
-            filename="simpledigiclock_p.kv") 
-        landscapeLayout = SimpleDigiClockLayout(
-            filename="simpledigiclock_l.kv") 
-        currentLayout   = landscapeLayout
+        super().__init__()
+        self.portraitLayout  = SimpleDigiClockPortrait() 
+        self.landscapeLayout = SimpleDigiClockLandscape()
+        self.currentLayout   = self.landscapeLayout
 
 
-    def on_brightness(self, isLight):
-        Logger.info("Brightness: " + isLight)
+    def on_brightness(self, context, isLight):
+        Logger.info("Brightness: " + str(isLight))
+        if isLight:
+            self.currentLayout.clockColor[3] = 1.0
+            self.currentLayout.frontColor[3] = 1.0
+        else:
+            self.currentLayout.clockColor[3] = 0.3
+            self.currentLayout.frontColor[3] = 0.2 
 
-    def on_direction(self, direction):
-        Logger.info("Direction: " + direction)
+    def on_direction(self, context, direction):
+        Logger.info("Direction: " + str(direction))
 
-    def on_time(self, newTime):
+    def on_time(self, context, newTime):
         '''
         Output new time with layout taken from the field content in the
         current layout
         '''
-        tm = newTime.strftime(currentLayout.clockFormat)
-        currentLayout.clockValue.text = tm
+        tm = newTime.strftime(self.currentLayout.clockFormat)
+        self.currentLayout.clockValue.text = tm
 
-    def on_date(self, newTime):
+    def on_date(self, context, newTime):
         '''
         Output new date with layout taken from the field content in the
         current layout
         '''
-        tm = newTime.strftime(currentLayout.dateFormat)
-        currentLayout.dateValue.text = tm
+        tm = newTime.strftime(self.currentLayout.dateFormat)
+        self.currentLayout.dateValue.text = tm
 
